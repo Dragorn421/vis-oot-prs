11254a08e28b0b006691868a0e502bbd7ecd502d
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-10-28 08:41:27 +0200

    try github api in action

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import sys
 from pathlib import Path
 
+import github
+
 print(sys.argv)
 
 runner_temp_dir = Path(sys.argv[1])
 
+g = github.Github(sys.argv[2])
+
 with (runner_temp_dir / "index.html").open("w") as f:
     f.write("Hello from python")
+
+    repo = g.get_repo("zeldaret/oot")
+    f.write(repo.full_name)
+    f.write(repo.description)
