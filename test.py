46e488bc46239701dbcce438ae142a4138bee3a3
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-10-28 08:08:58 +0200

    try with working in runner.temp dir with py script

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-with open("index.html", "w") as f:
+import sys
+from pathlib import Path
+
+print(sys.argv)
+
+runner_temp_dir = Path(sys.argv[1])
+
+with (runner_temp_dir / "index.html").open("w") as f:
     f.write("Hello from python")
